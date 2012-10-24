80fa036425ec63ed9fac38590c76de9476a12db3
theraju <theraju@gmail.com>
2012-10-24 01:44:57 -0400

    Basic hike details harvested and printed. Hash function works.

--- lib/tasks/crawler.rb
+++ lib/tasks/crawler.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 require 'anemone'
+require 'digest/sha1'
 
 Anemone.crawl("http://www.wta.org/go-hiking/hikes") do |anemone|
     anemone.on_every_page { |page| 
@@ -10,11 +11,12 @@ Anemone.crawl("http://www.wta.org/go-hiking/hikes") do |anemone|
         #features list: div id="filter-features", input name="features:list" innertext
         #regions and subregions: regex match the "select name=region part"
         
+        doc = Nokogiri::HTML(open(page.url))
+        
         if (page.url.to_s.scan(%r|hikes$|).length > 0)
             puts page.url
 
             #hike data: tr class="hike-row"
-            doc = Nokogiri::HTML(open(page.url))
             hikes_on_page = doc.css('tr[class=hike-row]')
             hikes_on_page.each { |hike|
 
@@ -22,6 +24,7 @@ Anemone.crawl("http://www.wta.org/go-hiking/hikes") do |anemone|
                #title: a class="hike-title" innertext
                title = hike.css('a.hike-title').first.content
                puts title
+               puts Digest::SHA1.hexdigest title
 
                #avg_rating: div class="current-rating" innertext
                avg_rating = hike.css('div.current-rating').first.content
@@ -41,14 +44,27 @@ Anemone.crawl("http://www.wta.org/go-hiking/hikes") do |anemone|
             }
         end
 
-
-
-        #stuff from the detail page
+        if (page.url.to_s.scan(%r|(http://www.wta.org/go-hiking/hikes/([a-zA-Z0-9\-]+))$|).length > 0)
+           #stuff from the detail page
            #description: div class="hike-full-description" innertext
+           detail_title = doc.css('h1.documentFirstHeading').first.content
+           puts detail_title
+           puts Digest::SHA1.hexdigest detail_title
+
+           desc = doc.css('div.hike-full-description').first.content
+
            #roundtrip, eleveation gain and high point: table class="stats-table" td label-cell is name, td data-cell is value
            #Labels are "Roundtrip", "Elevation gain" and "Highest point"
+           stats = doc.css('table.stats-table')
+           datacells = stats.css('td.data-cell')
 
+           if (3 == datacells.count)
+               puts datacells[0].content
+               puts datacells[1].content
+               puts datacells[2].content
+           end
 
+        end
     }
 
     anemone.focus_crawl { |page| 
