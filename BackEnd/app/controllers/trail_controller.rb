be2d9c69c5ed051595d64a74ad6ede82e0d6b9c4
theraju <theraju@gmail.com>
2012-11-24 23:10:12 -0500

    Flesh out searching for hikes in the backend. Fixes #1

--- BackEnd/app/controllers/trail_controller.rb
+++ BackEnd/app/controllers/trail_controller.rb
@@ -1,20 +1,39 @@
 class TrailController < ApplicationController
   def search
-    results = Trail.all
+#params are: roundtriplte, roundtripgte, elevgainlte, elevgaingte, ratinggte
+    results = Trail.where{ }
+
+    if (params.has_key?('roundtripgte') || params.has_key?('roundtriplte'))
+        results = results.where{ round_trip ^ nil }
+    end
     rt_gte = params[:roundtripgte].to_i
     if (rt_gte > 0)
-        results = Trail.where{ (round_trip ^ nil) & (round_trip >= rt_gte) }
+        results = results.where{ round_trip >= rt_gte }
     end
 
     rt_lte = params[:roundtriplte].to_i
-
     if (rt_lte > 0)
-        results = Trail.where{id.in(results.select{id})}.where{ (round_trip ^ nil ) & (round_trip <= rt_lte) }
+        results = results.where{ round_trip <= rt_lte }
     end
     
-    elevgain_lte = params[:elevgain].to_i
+    if (params.has_key?('elevgainlte') || params.has_key?('elevgaingte'))
+        results = results.where{ elevation_gain ^ nil }
+    end
+
+    elevgain_lte = params[:elevgainlte].to_i
     if (elevgain_lte > 0)
-        results = Trail.where{id.in(results.select{id})}.where{ (elevation_gain ^ nil) & (elevation_gain <= elevgain_lte) }
+        results = results.where{ elevation_gain <= elevgain_lte }
+    end 
+
+    elevgain_gte = params[:elevgaingte].to_i
+    if (elevgain_gte > 0)
+        results = results.where{ elevation_gain >= elevgain_gte }
+    end 
+
+    rating_gte = params[:ratinggte].to_i
+    if (rating_gte > 0)
+        results = results.where{ avg_rating ^ nil }
+        results = results.where{ avg_rating >= rating_gte }
     end 
 
     render :json => results
